2031933448757c0bc21b95b35d9084ad3bb591e0
Ruffa <ruffaresentes@me.com>
2026-06-27 00:42:37 +0800

    is food healthy

--- ch4_data_visualization/is_food_healthy.py
+++ ch4_data_visualization/is_food_healthy.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-import pandas as df
+import pandas as pd
 def format_plot():
     plt.xlabel('Public (%)')
     plt.ylabel('Experts (%)')
@@ -27,9 +27,26 @@ def add_labels(df, x_col, y_col, label_col):
         label = offset_spacing + row[label_col]
         plt.text(x, y, label, va='center', ha='left')
 
+df_public = pd.read_csv('healthy-food-survey-public.csv')
+df_public['public'] = df_public.eval('yes / (yes + no + no_opinion)')
+df_public['public'] = df_public.eval('public * 100').round()
+df_public = df_public[['food', 'public']]
+
+df_experts = pd.read_csv('healthy-food-survey-experts.csv')
+df_experts['experts'] = df_experts.eval('yes / (yes + no + no_opinion)')
+df_experts['experts'] = df_experts.eval('experts * 100').round()
+df_experts = df_experts[['food', 'experts']]
+
+df = df_public.merge(df_experts, on='food', how='left')
+
+
+df['public_minus_experts'] = df.eval('public - experts')
+df = df.sort_values(by='public_minus_experts', ascending=False)
+highest_disagreement = df.head(4)
 
 plt.scatter(df['public'], df['experts'], alpha=0.5) #alpha is transparency of dots
 format_plot()
 add_equality_line()
 square_the_plot()
 add_labels(highest_disagreement, 'public', 'experts', 'food')
+plt.show()
