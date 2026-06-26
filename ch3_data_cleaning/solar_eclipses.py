16ba54e0de69bc62bbd88b7dc741a3bd53783487
Ruffa <ruffaresentes@me.com>
2026-06-26 23:32:58 +0800

    found longest eclipse

--- ch3_data_cleaning/solar_eclipses.py
+++ ch3_data_cleaning/solar_eclipses.py
@@ -6,3 +6,20 @@ df = pd.read_csv('solar-eclipses.csv')
 ec = df[['date', 'duration']]
 # print(ec)
 
+ec['duration'] = ec['duration'].str.replace('s', '')
+print(ec)
+
+ec_duration = ec['duration'].str.split('m',  expand=True)
+ec_duration = ec_duration.dropna()
+ec_duration = ec_duration.astype('int')
+print(ec_duration)
+
+ec[['m', 's']] = ec_duration
+# print(ec)
+
+ec = ec.dropna()
+ec['total_seconds'] = ec.eval('m * 60 + s')
+print(ec)
+print(ec.query('total_seconds == total_seconds.max()'))
+
+
