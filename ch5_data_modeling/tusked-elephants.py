cc62a3a3cf9b5ed307f2ed7246ede624ee0c417e
Ruffa <ruffaresentes@me.com>
2026-06-27 14:45:44 +0800

    scatter plot pre poaching and recovery data

--- ch5_data_modeling/tusked-elephants.py
+++ ch5_data_modeling/tusked-elephants.py
@@ -9,3 +9,17 @@ post_recovery = df.query('period == "2005-13"')
 
 # print("Before:", pre_poaching['tusk_length'].mean())
 # print("After:", post_recovery['tusk_length'].mean())
+
+"""
+Activity Goals:
+Plot tusk length vs shoulder height for both groups
+Use a triangle marker for pre_poaching.
+Use a square marker for post_recovery.
+"""
+plt.scatter(pre_poaching['shoulder_height'], pre_poaching['tusk_length'], marker='^')
+plt.scatter(post_recovery['shoulder_height'], post_recovery['tusk_length'], marker='s')
+
+plt.xlabel('Shoulder Height (cm)')
+plt.ylabel('Tusk Length (cm)')
+plt.text(x=200, y=120, s='Pre-poaching', color='C0')
+plt.text(x=220, y=35, s='Post-recovery', color='C1')
